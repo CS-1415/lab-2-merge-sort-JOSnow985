bb43bbb68a23e604575167005b53dd8303698160
JOSnow985 <jaden.olvera@students.snow.edu>
2026-01-20 17:05:00 -0700

    Initial TDD and stub

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+// Jaden Olvera Lab 2 - Merge Sort
+
+CombineSortedTest();
+// Should only be reachable if the tests have passed.
+Console.WriteLine("Tests passed.");
+
+
+// Takes two sorted lists and returns a sorted list that includes all values from a and b
+// Not Recursive
+static int[] CombineSortedArrays(int[] a, int[] b)
+{
+    return [];
+}
+
+// Test method for non-Recursive sorted list combine
+static void CombineSortedTest()
+{
+    Debug.Assert(Enumerable.SequenceEqual(
+        CombineSortedArrays(
+            [-5, 2, 5, 8, 10], 
+            [1, 2, 5]
+            ),
+            [-5, 1, 2, 2, 5, 5, 8, 10]
+        ));
+}
